7b0892042c4dbdcce99d12011eced73b3296e8c6
devanshivatss <devanshivatsind@gmail.com>
2025-04-27 18:53:07 +0200

    Update Navbar.jsx
    Commented out certifications from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,9 +17,9 @@ function Navbar() {
         <li><a href="#home">Home</a></li>
         <li><a href="#education">Education</a></li>
         <li><a href="#insights">Projects</a></li>
-        <li><a href="#experience">Experience</a></li>
+        <li><a href="#experience">Experiences</a></li>
         <li><a href="#skills">Skills</a></li>
-        <li><a href="#certifications">Certifications</a></li>
+        {/* <li><a href="#certifications">Certifications</a></li> */}
         <li><a href="#projects">Industry Insights</a></li>
         {/* <li><a href="#linkedin">Posts</a></li> */}
         <li><a href="#contact">Contact</a></li>
